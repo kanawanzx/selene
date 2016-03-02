45b63c9a1b13360b2e97c7287ef1e1b102d164a4
Hy Hý <tronghyt1@gmail.com>
2016-03-02 18:14:14 +0700

    pun

--- 404.php
+++ 404.php
@@ -8,6 +8,7 @@ get_header();
 ?>
 <!-- 404 PAGE
 ======================================================================== -->
+không có gì hết
 <div class="as-page-wrapper">
     <div class="as-content-wrapper">
         <div class="as-wrapper clearfix as-wrapper-page-404">
