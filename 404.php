5fc20c467d9871909cd062f52d0af03f1547f736
Hy Hý <tronghyt1@gmail.com>
2016-03-02 18:11:00 +0700

    delete

--- 404.php
+++ 404.php
@@ -8,8 +8,6 @@ get_header();
 ?>
 <!-- 404 PAGE
 ======================================================================== -->
-
-kjsdgfz eqietrearyoW UR
 <div class="as-page-wrapper">
     <div class="as-content-wrapper">
         <div class="as-wrapper clearfix as-wrapper-page-404">
